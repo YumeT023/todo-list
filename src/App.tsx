2f04937d924e5d2333a522ac6ed3b86e9b79c7c8
Yume <hei.hajatiana@gmail.com>
2023-01-31 01:45:57 +0300

    feat: allow to create todo and edit its status

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,104 @@
-import React from "react";
+import React, { ChangeEvent, FormEvent, useEffect, useState } from "react";
+import { TodoList } from "./models/Todo.model";
+import { Container, Item, TextInput } from "./components";
+import { mapToProp } from "./utils/map-to-prop";
 import "./App.css";
 
 const App = () => {
-  return <h1>init project</h1>;
+  const [state, setState] = useState<TodoList>([]);
+  const [inputVal, setInputVal] = useState("");
+  const [lastIdx, setLastIdx] = useState(0);
+
+  useEffect(() => console.log(lastIdx), [lastIdx]);
+
+  useEffect(() => {
+    const _performLastIndexFind = () => {
+      const indexes = mapToProp(state, "idx");
+      const last = Math.max(...indexes, lastIdx);
+      setLastIdx(last);
+    };
+
+    _performLastIndexFind();
+  }, []);
+
+  const handleCheck = (idx: number) => {
+    const _state = state.slice();
+    const _idx = state.findIndex((item) => item.idx === idx);
+
+    _state[_idx] = { ..._state[_idx], isComplete: true };
+
+    setState(_state);
+  };
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setInputVal(value);
+  };
+
+  const _resetInputValue = () => setInputVal("");
+
+  const _createTodo = (content: string) => {
+    setLastIdx((prevLast) => prevLast + 1);
+
+    return {
+      content,
+      idx: lastIdx,
+      isComplete: false,
+    };
+  };
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+
+    const _todo = _createTodo(inputVal);
+
+    setState((prevState) => prevState.concat(_todo));
+
+    _resetInputValue();
+  };
+
+  return (
+    <Container className="container container--flex-and-center-all">
+      <Container className="container__box rounded--box">
+        <header className="container__header container--flex-and-center-y">
+          <h2>__init</h2>
+        </header>
+
+        <main className="container__main">
+          <section className="container__section">
+            <form
+              className="container__form container--flex-and-center-all"
+              spellCheck={false}
+              onSubmit={handleSubmit}
+            >
+              <TextInput
+                placeholder="enter a todo"
+                value={inputVal}
+                onChange={handleChange}
+              />
+            </form>
+
+            <div className="container__list">
+              {state.map(
+                (item) =>
+                  !item.isComplete && (
+                    <Item item={item} onChange={() => handleCheck(item.idx)} />
+                  )
+              )}
+            </div>
+          </section>
+
+          <section className="container__section">
+            <div className="container__list">
+              {state.map((item) => {
+                return item.isComplete && <Item item={item} />;
+              })}
+            </div>
+          </section>
+        </main>
+      </Container>
+    </Container>
+  );
 };
 
 export default App;
