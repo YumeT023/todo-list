ea00fb16dbcc2a1547092dfd4ae082b2a8e32e4c
Yume <hei.hajatiana@gmail.com>
2023-01-31 01:43:21 +0300

    chore(layout): add item

---
+++ src/components/item/Item.tsx
@@ -0,0 +1,23 @@
+import { TodoItem } from "../../models/Todo.model";
+import "./Item.css";
+
+export type ItemProps = {
+  item: TodoItem;
+  onChange?(): void;
+};
+
+export const Item = ({ item, onChange = () => null }: ItemProps) => {
+  const _getStatusClass = () =>
+    `item--${item.isComplete ? "done" : "unfinished"}`;
+
+  return (
+    <div className={`item ${_getStatusClass()}`}>
+      <div className="item__content">{item.content}</div>
+      <div className="item__action">
+        {!item.isComplete && (
+          <input type="checkbox" name="item-status" onChange={onChange} />
+        )}
+      </div>
+    </div>
+  );
+};
